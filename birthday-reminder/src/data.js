6173693f172cb3f4d344958914a0bd2d18c21165
Kartikey <kartikeygupta9599@gmail.com>
2024-04-12 21:43:19 +0530

    birthday-reminder

---
+++ birthday-reminder/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Thomas Shelby",
+    age: 47,
+    image:
+      "https://imgs.search.brave.com/0ejOPo1E1odMFQUMDtWOMGV4S_GUze9J7kchwrH5lNg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9md21l/ZGlhLmZhbmRvbXdp/cmUuY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDI0LzAzLzE4/MjMyNDA5L2NtcGIu/d2VicA",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
