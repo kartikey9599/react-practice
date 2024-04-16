8a3011ebe662070d76dc4486e81bc11ef465fee5
Kartikey <kartikeygupta9599@gmail.com>
2024-04-16 14:55:22 +0530

    review-app

---
+++ slider/src/data.js
@@ -0,0 +1,36 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://imgs.search.brave.com/iBCeUIjk5sraQJhjDOnnrnzbc36J9GNtg1ZGcxTXUzg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzlmL2Zh/L2E0LzlmZmFhNDYy/MjQxMzc5M2MxYTRm/OGZkODIyYjlkMWNi/LmpwZw",
+    name: "Kartikey Gupta",
+    title: "Dead Man",
+    quote: "In the bleak Mid-Winter.",
+  },
+  {
+    id: 2,
+    image:
+      "https://imgs.search.brave.com/Izd0LA5-9k3Oa2fZodKNH3MKggKmH-8bdE69FZnDZaw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1Qk1qQTVOVFkx/TmprMk1GNUJNbDVC/YW5CblhrRnRaVGd3/TWpZNU56TTRNakVA/LmpwZw",
+    name: "john shelby",
+    title: "Lover",
+    quote: "Are you laughing at my brother?",
+  },
+  {
+    id: 3,
+    image:
+      "https://imgs.search.brave.com/7FGgiBULkXmcfuvScqT14xhG9gIM0G5ukSkukHV1gWM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuaW1tZWRpYXRl/LmNvLnVrL3Byb2R1/Y3Rpb24vdm9sYXRp/bGUvc2l0ZXMvMy8y/MDE3LzExL3BlYWt5/LWFydGh1ci1lNjg4/MWQ0LmpwZz9xdWFs/aXR5PTkwJmZpdD02/MjAsNDEz",
+    name: "Author Shelby",
+    title: "Devil's favourite",
+    quote: "By the order of fooking peaky blinders.",
+  },
+  {
+    id: 4,
+    image:
+      "https://imgs.search.brave.com/8o82isM2ZianEjcgDwuv5YButSBZw_XTXmTRT6KsCkQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLnNyY2RuLmNv/bS93b3JkcHJlc3Mv/d3AtY29udGVudC91/cGxvYWRzLzIwMjAv/MTIvUGVha3ktQmxp/bmRlcnMtMTAtU2Fk/ZGVzdC1UaGluZ3Mt/QWJvdXQtR3JhY2Ut/U2hlLVdhbnRlZC1U/by1NYWtlLUhlci1G/YXRoZXItUHJvdWQu/anBn",
+    name: "Grace",
+    title: "My love",
+    quote: "Happy or Sad? ",
+  },
+];
+
+export default people;
