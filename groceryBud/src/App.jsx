6a895a6e425538b66f4029651fd3bce55492373e
Kartikey <kartikeygupta9599@gmail.com>
2024-04-19 18:07:48 +0530

    grocery-Bud

---
+++ groceryBud/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import List from "./List";
+import Alert from "./Alert";
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+    if (!name) {
+      showAlert(true, "danger", "please enter value");
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+  return (
+    <>
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && <Alert {...alert} />}
+          <h3>grocery bud</h3>
+          <div className="form-control">
+            <input
+              type="text"
+              className="grocery"
+              placeholder="e.g. milk"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "edit" : "submit"}
+            </button>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} />
+            <button className="clear-btn">clear items</button>
+          </div>
+        )}
+      </section>
+    </>
+  );
+}
+
+export default App;
